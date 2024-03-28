fcc7d5d2d9efd29c14348175ccc1d1d0183dbdf6
CSingogo <singogosingogo@gmail.com>
2024-03-29 01:58:53 +0200

    bottom navigation bar completed

--- flutter_whatapp_clone/lib/main.dart
+++ flutter_whatapp_clone/lib/main.dart
@@ -275,6 +275,34 @@ class MainApp extends StatelessWidget {
             color: Colors.white,)
             ),
             
+            bottomNavigationBar: BottomNavigationBar(
+              showUnselectedLabels: true,
+              currentIndex: 0,
+              elevation: 1.0,
+              selectedItemColor: Colors.black,
+              unselectedItemColor: const Color.fromARGB(255, 134, 119, 119),
+              selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+              unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+        items: const [
+          BottomNavigationBarItem(
+            label: "Chats",
+            icon: Icon(Icons.sms),
+          ),
+          BottomNavigationBarItem(
+            label: "Updates",
+            icon: Icon(Icons.restore),
+          ),
+          BottomNavigationBarItem(
+            label: "Communities",
+            icon: Icon(Icons.accessibility_new_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: "Calls",
+            icon: Icon(Icons.call),
+          ),
+        ],
+         
+        onTap: (int indexOfItem) {}),
         ),
         
     );

7c803b8629fc51667a017bc6b5ac1e93e6ad29d3
CSingogo <singogosingogo@gmail.com>
2024-03-29 03:27:29 +0200

    refactoring and clean up

---
+++ flutter_whatapp_clone/lib/top.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.teal[900],
+      title:  Row(
+        children: [
+          Text(
+            'WhatsApp',
+             style: TextStyle(
+              color: Colors.white,
+               fontSize: 30),
+             ),
+             Spacer(),
+          Icon(
+            Icons.add_a_photo_outlined,
+            size:20,
+            color: Colors.white,),
+            SizedBox(width: 30),
+          Icon(
+            Icons.search,
+            size:20,
+            color: Colors.white,
+          ),
+          SizedBox(width: 30),
+          Icon(
+            Icons.more_vert,
+            size:20,
+            color: Colors.white,
+          )
+        ],)
+    );
+  }
+}
